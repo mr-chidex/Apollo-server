fd5ee700c0fc61b26d860b69ba18ed0543c46a35
mr-chidex <mbahchiboy2@gmail.com>
2022-07-26 10:06:23 +0100

    added mock db

---
+++ src/mocks/db.ts
@@ -0,0 +1,62 @@
+export default [
+  {
+    id: 1,
+    name: 'James',
+    age: 23,
+    married: false,
+  },
+  {
+    id: 2,
+    name: 'Anita',
+    age: 25,
+    married: true,
+  },
+  {
+    id: 3,
+    name: 'Brenda',
+    age: 23,
+    married: true,
+  },
+  {
+    id: 4,
+    name: 'Paul',
+    age: 28,
+    married: true,
+  },
+  {
+    id: 5,
+    name: 'Terry',
+    age: 29,
+    married: false,
+  },
+  {
+    id: 6,
+    name: 'Jessica',
+    age: 25,
+    married: true,
+  },
+  {
+    id: 7,
+    name: 'Andrew',
+    age: 20,
+    married: false,
+  },
+  {
+    id: 8,
+    name: 'Ahmed',
+    age: 25,
+    married: true,
+  },
+  {
+    id: 9,
+    name: 'Glory',
+    age: 23,
+    married: true,
+  },
+  {
+    id: 10,
+    name: 'Gray',
+    age: 31,
+    married: true,
+  },
+];
